e399cd528f4087fcffb0410bdd0321a5f5bdcd94
mrraxa01 <mrraxa01@gmail.com>
2020-09-22 23:08:16 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -86,6 +89,49 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT seller.*,department.Name as DepName "
+				+ "FROM seller INNER JOIN department "
+				+ "ON seller.DepartmentId = department.Id "
+				+ "WHERE DepartmentId = ? "
+				+ "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			//List para salvar os vendedores do departamento
+			List <Seller> list = new ArrayList<>();
+			//O Map vai restringir que o departamento seja instanciado para cada elemento da lista
+			Map<Integer, Department> map = new HashMap<>();
+			
+			//while vai funcionar enquanto tiver dados no rs
+			while (rs.next()) {
+				//Instancia o departamento no map, se não existir vai retornar nulo
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				//se o map não retornar nulo, vai pular o if e nã vai instanciar o dep novamente
+				if( dep == null) {
+					dep =instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instanciateSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			return list;
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}	
+	
 	}
 
 }
