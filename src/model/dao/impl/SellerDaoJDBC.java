591d584a8602052cb356d60d338de66132233ce2
mrraxa01 <mrraxa01@gmail.com>
2020-09-23 20:45:00 -0300

    Insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,6 +1,7 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,6 +9,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
@@ -24,7 +27,44 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+"VALUES "
+					+"(?, ?, ?, ?, ?) ",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			//para verificar se a inserção obteve sucesso, vamos verrificar
+			//se foi criado um id para objeto e já devolver o obj com o id 
+			//gerado pelo banco de dados
+			
+			if(rowsAffected >0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1); // a chave gerada vem na coluna 1
+					obj.setId(id);
+					//obj.setId(rs.getInt(1)); poderia ser assim direto
+					DB.closeResultSet(rs);
+				}
+			}else {
+				throw new DbException("Unexpected error! No rows affected");
+			}
+		}catch(SQLException e){
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
