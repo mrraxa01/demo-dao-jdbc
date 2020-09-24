3f6fb51382516933c2a1df500acca1eb7a00dbe4
mrraxa01 <mrraxa01@gmail.com>
2020-09-23 23:51:35 -0300

    Department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -9,17 +9,21 @@ public class Program2 {
 	public static void main(String[] args) {
 		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
-				
-		/*System.out.println("\n==== TEST 1 : department insert ====\n");
+		/*		
+		System.out.println("\n==== TEST 1 : department insert ====\n");
 		Department newDep = new Department(null, "Food");
 		depDao.insert(newDep);
 		System.out.println("Inserted!! New ID department = " + newDep.getId());
-		*/
+		
 		System.out.println("\n==== TEST 2 : department update ====\n");
 		Department dep = new Department(6,"Tools");
 		depDao.update(dep);
 		System.out.println("Update completed!");
-		
+		*/
+		System.out.println("\n==== TEST 3 : department delete ====\n");
+		//Department dep = new Department();
+		depDao.deleteById(11);
+		System.out.println("Delete OK!");
 
 	}
 
