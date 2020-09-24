d7199bd5e807696f1e55a3897d34430554f9fa28
mrraxa01 <mrraxa01@gmail.com>
2020-09-23 23:39:35 -0300

    Update department implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,7 +39,7 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted!! New ID = " + newSeller.getId());
 		
-		System.out.println("\n==== TEST 5 : seller insert ====\n");
+		System.out.println("\n==== TEST 5 : seller update ====\n");
 		seller = sellerDao.findById(1);
 		seller.setBaseSalary(3500.00);
 		sellerDao.update(seller);
--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,10 +10,16 @@ public class Program2 {
 		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 				
-		System.out.println("\n==== TEST 1 : seller insert ====\n");
+		/*System.out.println("\n==== TEST 1 : department insert ====\n");
 		Department newDep = new Department(null, "Food");
 		depDao.insert(newDep);
 		System.out.println("Inserted!! New ID department = " + newDep.getId());
+		*/
+		System.out.println("\n==== TEST 2 : department update ====\n");
+		Department dep = new Department(6,"Tools");
+		depDao.update(dep);
+		System.out.println("Update completed!");
+		
 
 	}
 
